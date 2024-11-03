71a57e83571e52dc8a64e7a53dbe701c57a8dbf2
PradeshaP <ponnuranganpradesha@gmail.com>
2024-11-03 21:49:09 +0530

    Add files via upload

---
+++ rearrange_array_elements_by_sign.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        int pos=0;
+        int neg=1;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0)
+           {
+            arr[pos]=nums[i];
+            pos+=2;
+           }
+           else
+           {
+            arr[neg]=nums[i];
+            neg+=2;
+           }
+        }
+        
+       return arr;
+    }
+};
